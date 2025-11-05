5781e3bd8393d0a3c9e1e6a9bf21d12ac1543e09
AleenAlghamdi <s202244260@kfupm.edu.sa>
2025-11-05 12:13:01 +0300

    TODO 4: Create Math Operation Functions (in operations.js)

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,26 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((result, num, index) => {
+      if (index === 0) return num;
+      return result - num;
+  }, 0);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((product, num) => product * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((result, num, index) => {
+      if (index === 0) return num;
+      if (num === 0) {
+          console.log("Error: Division by zero is not allowed");
+          return NaN;
+      }
+      return result / num;
+  }, 1);
 }
 
